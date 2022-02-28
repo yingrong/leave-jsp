bd7a42b0a759e1bd011c17bb71f62a3dffdc3486
wanshifu <ninputerwonder@outlook.com>
2022-02-28 16:27:41 +0800

    对保存，选中和取消'活动'添加happy path测试

--- src/test/java/com/tw/api_mantenance/after/TeamBuildingServiceTest.java
+++ src/test/java/com/tw/api_mantenance/after/TeamBuildingServiceTest.java
@@ -5,13 +5,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TeamBuildingServiceTest {
@@ -22,7 +23,7 @@ public class TeamBuildingServiceTest {
     TeamBuildingService teamBuildingService;
 
     @Before
-    void init() {
+    public void init() {
         mockTeamBuildingPackageItemRepository = mock(TeamBuildingPackageItemRepository.class);
         mockTeamBuildingPackageRepository = mock(TeamBuildingPackageRepository.class);
         mockActivityRepository = mock(ActivityRepository.class);
@@ -31,7 +32,6 @@ public class TeamBuildingServiceTest {
 
     @Test
     public void testQueryTeamBuildingPackageItemById() {
-
         long teamBuildingPackageItemId = 1234223L;
 
         TeamBuildingPackageItem teamBuildingPackageItem = new TeamBuildingPackageItem(teamBuildingPackageItemId, 101L, Arrays.asList(
@@ -45,23 +45,86 @@ public class TeamBuildingServiceTest {
                 new Activity(14L, "第二个测试活动")
         ));
 
-
         TeamBuildingPackageItemDto teamBuildingPackageItemDto = teamBuildingService.queryTeamBuildingPackage(teamBuildingPackageItemId);
-
-//        private Long id;
-//        private Long packageId;
-//        private String name;
-//        private List<ActivityItemDto> activityItemDtos;
-
-//        private Long id;
-//        private Long activityId;
-//        private String name;
-//        private Integer count;
-//        private Boolean selected;
 
         Assert.assertEquals(teamBuildingPackageItemId, teamBuildingPackageItemDto.getId().longValue());
         Assert.assertEquals(101L, teamBuildingPackageItemDto.getPackageId().longValue());
+        Assert.assertEquals("团建测试包", teamBuildingPackageItemDto.getName());
 
-        Assert.assertTrue(true);
+        Assert.assertEquals(2, teamBuildingPackageItemDto.getActivityItemDtos().size());
+
+        ActivityItemDto activityItemDto = teamBuildingPackageItemDto.getActivityItemDtos().stream().filter(i -> i.getId() == 11L).findFirst().get();
+        Assert.assertEquals(13L, activityItemDto.getActivityId().longValue());
+        Assert.assertEquals("测试活动", activityItemDto.getName());
+        Assert.assertEquals(5, activityItemDto.getCount().intValue());
+        Assert.assertTrue(activityItemDto.getSelected());
+
+        activityItemDto = teamBuildingPackageItemDto.getActivityItemDtos().stream().filter(i -> i.getId() == 12L).findFirst().get();
+        Assert.assertEquals(14L, activityItemDto.getActivityId().longValue());
+        Assert.assertEquals("第二个测试活动", activityItemDto.getName());
+        Assert.assertNull(activityItemDto.getCount());
+        Assert.assertFalse(activityItemDto.getSelected());
+    }
+
+    @Test
+    public void testSelectActivityItem() {
+        long teamBuildingPackageItemId = 1234223L;
+
+        TeamBuildingPackageItem teamBuildingPackageItem = new TeamBuildingPackageItem(teamBuildingPackageItemId, 101L, Arrays.asList(
+                new ActivityItem(11L, 13L, true, 5),
+                new ActivityItem(12L, 14L, false, null)));
+
+        when(mockTeamBuildingPackageItemRepository.findById(teamBuildingPackageItemId)).thenReturn(teamBuildingPackageItem);
+
+        teamBuildingService.selectActivityItem(teamBuildingPackageItemId, 12L, 10);
+
+        ArgumentCaptor<TeamBuildingPackageItem> argumentCaptor = ArgumentCaptor.forClass(TeamBuildingPackageItem.class);
+        verify(mockTeamBuildingPackageItemRepository, times(1)).save(argumentCaptor.capture());
+
+        TeamBuildingPackageItem captorValue = argumentCaptor.getValue();
+        Assert.assertEquals(teamBuildingPackageItemId, captorValue.getId().longValue());
+        Assert.assertEquals(101L, captorValue.getPackageId().longValue());
+        Assert.assertEquals(2, captorValue.getActivityItems().size());
+
+        ActivityItem activityItem = captorValue.getActivityItems().stream().filter(i -> i.getId() == 11L).findFirst().get();
+        Assert.assertEquals(13L, activityItem.getActivityId().longValue());
+        Assert.assertEquals(5, activityItem.getCount().intValue());
+        Assert.assertTrue(activityItem.getSelected());
+
+        activityItem = captorValue.getActivityItems().stream().filter(i -> i.getId() == 12L).findFirst().get();
+        Assert.assertEquals(14L, activityItem.getActivityId().longValue());
+        Assert.assertEquals(10, activityItem.getCount().intValue());
+        Assert.assertTrue(activityItem.getSelected());
+    }
+
+    @Test
+    public void testUnselectActivityItem() {
+        long teamBuildingPackageItemId = 1234223L;
+
+        TeamBuildingPackageItem teamBuildingPackageItem = new TeamBuildingPackageItem(teamBuildingPackageItemId, 101L, Arrays.asList(
+                new ActivityItem(11L, 13L, true, 5),
+                new ActivityItem(12L, 14L, false, null)));
+
+        when(mockTeamBuildingPackageItemRepository.findById(teamBuildingPackageItemId)).thenReturn(teamBuildingPackageItem);
+
+        teamBuildingService.unSelectActivityItem(teamBuildingPackageItemId, 11L);
+
+        ArgumentCaptor<TeamBuildingPackageItem> argumentCaptor = ArgumentCaptor.forClass(TeamBuildingPackageItem.class);
+        verify(mockTeamBuildingPackageItemRepository, times(1)).save(argumentCaptor.capture());
+
+        TeamBuildingPackageItem captorValue = argumentCaptor.getValue();
+        Assert.assertEquals(teamBuildingPackageItemId, captorValue.getId().longValue());
+        Assert.assertEquals(101L, captorValue.getPackageId().longValue());
+        Assert.assertEquals(2, captorValue.getActivityItems().size());
+
+        ActivityItem activityItem = captorValue.getActivityItems().stream().filter(i -> i.getId() == 11L).findFirst().get();
+        Assert.assertEquals(13L, activityItem.getActivityId().longValue());
+        Assert.assertNull(activityItem.getCount());
+        Assert.assertFalse(activityItem.getSelected());
+
+        activityItem = captorValue.getActivityItems().stream().filter(i -> i.getId() == 12L).findFirst().get();
+        Assert.assertEquals(14L, activityItem.getActivityId().longValue());
+        Assert.assertNull(activityItem.getCount());
+        Assert.assertFalse(activityItem.getSelected());
     }
 }

443935012b3229cde0e4290771ffa8727b94a9af
yingrong <yingrong.hu@gmail.com>
2022-05-19 16:09:06 +0800

    新增 jsp 自定义标签 InputTextTag 及其测试

---
+++ src/main/java/com/tw/customer_tag/InputTextTag.java
@@ -0,0 +1,67 @@
+package com.tw.customer_tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import java.io.IOException;
+
+public class InputTextTag extends BodyTagSupport {
+
+    private String id;
+    private String name;
+    private String value;
+    private String onblur;
+
+    @Override
+    public int doStartTag() throws JspException {
+        JspWriter out = getJspWriter();
+
+        String inputTextString = buildInputText();
+        try {
+            out.println(inputTextString );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return EVAL_BODY_INCLUDE;
+    }
+
+    private String buildInputText() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("<input type=\"text\" ")
+                .append(" id=\""+ id + "\" ")
+                .append(" name=\""+ name + "\" ");
+
+        if (null != value) {
+            builder.append(" value=\""+ value + "\" ");
+        }
+
+        if (null != onblur) {
+            builder.append("onblue=\"" + onblur + "\" ");
+        }
+
+        builder.append(">");
+        return builder.toString();
+    }
+
+    JspWriter getJspWriter() {
+        return pageContext.getOut();
+
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setOnblur(String onblue) {
+        this.onblur = onblue;
+    }
+}
---
+++ src/test/java/com/tw/customer_tag/InputTextTagTest.java
@@ -0,0 +1,64 @@
+package com.tw.customer_tag;
+
+import org.junit.Test;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+public class InputTextTagTest {
+
+    @Test
+    public void should_write_input_text() throws JspException, IOException {
+        // given
+        JspWriter jspWriter = mock(JspWriter.class);
+        InputTextTag inputTextTag = spy(InputTextTag.class);
+        inputTextTag.setId("id1");
+        inputTextTag.setName("name1");
+        inputTextTag.setValue("val1");
+        doReturn(jspWriter).when(inputTextTag).getJspWriter();
+
+        // when
+        inputTextTag.doStartTag();
+
+        // then
+        verify(jspWriter).println("<input type=\"text\"  id=\"id1\"  name=\"name1\"  value=\"val1\" >");
+    }
+
+    @Test
+    public void should_write_input_text_without_value() throws JspException, IOException {
+        // given
+        JspWriter jspWriter = mock(JspWriter.class);
+        InputTextTag inputTextTag = spy(InputTextTag.class);
+        inputTextTag.setId("id1");
+        inputTextTag.setName("name1");
+        doReturn(jspWriter).when(inputTextTag).getJspWriter();
+
+        // when
+        inputTextTag.doStartTag();
+
+        // then
+        verify(jspWriter).println("<input type=\"text\"  id=\"id1\"  name=\"name1\" >");
+    }
+
+    @Test
+    public void should_write_onblur() throws JspException,IOException {
+        // given
+        JspWriter jspWriter = mock(JspWriter.class);
+        InputTextTag inputTextTag = spy(InputTextTag.class);
+        inputTextTag.setId("id1");
+        inputTextTag.setName("name1");
+        inputTextTag.setOnblur("onblur()");
+        doReturn(jspWriter).when(inputTextTag).getJspWriter();
+
+        // when
+        inputTextTag.doStartTag();
+
+        // then
+        verify(jspWriter).println("<input type=\"text\"  id=\"id1\"  name=\"name1\" onblue=\"onblur()\" >");
+    }
+
+
+}

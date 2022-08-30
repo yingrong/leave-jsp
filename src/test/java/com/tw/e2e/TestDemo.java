37dc7fc680cbd84715358d4b41e11146156c4d6b
yingrong <yingrong.hu@gmail.com>
2022-08-30 17:56:56 +0800

    test selenium chrome headless

--- src/test/java/com/tw/e2e/TestDemo.java
+++ src/test/java/com/tw/e2e/TestDemo.java
@@ -23,6 +23,7 @@ public class TestDemo {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 

a145f4c727fe617f2dc58704b3bf68491557dd2c
yingrong <yingrong.hu@gmail.com>
2022-08-30 17:19:12 +0800

    新增 selenium 测试

---
+++ docs/selenium.md
@@ -0,0 +1,23 @@
+# 使用 selenium 做 e2e 测试
+
+## 安装
+
+### 1.Install a Selenium library and web driver manager
+
+```xml
+<dependencys>
+    <dependency>
+        <groupId>org.seleniumhq.selenium</groupId>
+        <artifactId>selenium-java</artifactId>
+        <version>4.4.0</version>
+    </dependency>
+    <dependency>
+        <groupId>io.github.bonigarcia</groupId>
+        <artifactId>webdrivermanager</artifactId>
+        <version>5.3.0</version>
+        <scope>test</scope>
+    </dependency>
+</dependencys>
+```
+### 2.Install browser drivers
+
---
+++ src/test/java/com/tw/e2e/TestDemo.java
@@ -0,0 +1,43 @@
+package com.tw.e2e;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setUpTest() {
+        driver = new ChromeDriver();
+    }
+
+
+    @Test
+    @Ignore
+    public void should_use_chrome() {
+        System.setProperty("webdriver.chrome.driver","/Users/somebody/bin/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.baidu.com/");
+
+        driver.quit();
+    }
+
+    @Test
+    public void should_test_user_web_driver_manager() {
+        driver.get("https://www.baidu.com/");
+
+        driver.quit();
+    }
+}

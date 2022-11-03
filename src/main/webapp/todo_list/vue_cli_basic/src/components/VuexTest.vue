d9d8b0e635a7e98c1825cde38131d2ec733242ba
yingrong <yingrong.hu@gmail.com>
2022-11-03 14:44:28 +0800

    新增 子组件不影响测试

--- src/main/webapp/todo_list/vue_cli_basic/src/components/VuexTest.vue
+++ src/main/webapp/todo_list/vue_cli_basic/src/components/VuexTest.vue
@@ -6,14 +6,24 @@
       <span data-testid="count-value">{{ count }}</span>
       <button data-testid="incrementer" @click="increment">+</button>
     </div>
+    <HelloWorld :msg="hix"></HelloWorld>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
+import HelloWorld from "@/components/HelloWorld";
 export default {
+  data() {
+    return {
+      hix: " helllo vuex"
+    }
+  },
   computed: {
     ...mapState(['count'])
+  },
+  components: {
+    HelloWorld,
   },
   methods: {
     ...mapActions(['decrement', 'increment'])

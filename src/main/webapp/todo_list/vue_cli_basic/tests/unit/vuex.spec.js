f225108faf0397efea72158857c8c8eefcdbdbd8
yingrong <yingrong.hu@gmail.com>
2022-11-03 14:26:01 +0800

    新增 vuex 及其测试

---
+++ src/main/webapp/todo_list/vue_cli_basic/tests/unit/vuex.spec.js
@@ -0,0 +1,57 @@
+// import '@testing-library/jest-dom'
+import {render, fireEvent} from '@testing-library/vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import VuexTest from '../../src/components/VuexTest'
+import {store} from '../../src/store/store'
+
+describe('test vuex', () => {
+    it('should render components with vuex', async () => {
+        // This is a silly store that can never be changed.
+        // eslint-disable-next-line no-shadow
+        const store = {
+            state: {count: 1000},
+            actions: {
+                increment: () => jest.fn(),
+                decrement: () => jest.fn(),
+            },
+        }
+
+        // Notice how here we are not using the helper method, because there's no
+        // need to do that.
+        const { getByText} = render(VuexTest, {store})
+
+        // await fireEvent.click(getByText('+'))
+        // expect(getByTestId('count-value')).toHaveTextContent('1000')
+        //
+        // await fireEvent.click(getByText('-'))
+        // expect(getByTestId('count-value')).toHaveTextContent('1000')
+    });
+
+    it('should render2', async () => {
+        Vue.use(Vuex)
+
+        const { getByText} = render(VuexTest, {
+            store: new Vuex.Store({
+                state: {count: 3},
+                mutations: {
+                    increment(state) {
+                        state.count++
+                    },
+                    decrement(state) {
+                        state.count--
+                    },
+                },
+                actions: {
+                    increment(context) {
+                        context.commit('increment')
+                    },
+                    decrement(context) {
+                        context.commit('decrement')
+                    },
+                },
+            }),
+        })
+    });
+})

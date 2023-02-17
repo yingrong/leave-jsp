eafeed12d24666da56535c128298685f85b2a5b3
Edward NoaLand <edwardnoaland@gmail.com>
2023-02-17 11:00:39 +0800

    feat: add mutation test framework -- stryker

---
+++ src/main/webapp/todo_list/vue_cli_basic/stryker.conf.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please see the guide for more information: https://stryker-mutator.io/docs/stryker-js/guides/vuejs",
+  testRunner: "jest",
+  mutator: {
+    plugins: [],
+  },
+  jest: {},
+  reporters: ["progress", "clear-text", "html"],
+  coverageAnalysis: "off",
+};
+export default config;
